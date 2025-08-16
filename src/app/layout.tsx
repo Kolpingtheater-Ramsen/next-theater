aae1e221558c972c7027de68078cca1cc4355325
LoggeL <hyper.xjo@gmail.com>
2025-08-16 17:00:50 +0200

    Add social media links for YouTube and Instagram in layout and home page sections

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,6 +76,46 @@ export default function RootLayout({
             <span>
               © {new Date().getFullYear()} Kolping-Open-Air-Theater Ramsen
             </span>
+            <div className='flex items-center gap-4'>
+              <a
+                href='https://www.youtube.com/@kolpingtheaterramsen'
+                target='_blank'
+                rel='noreferrer'
+                className='inline-flex items-center gap-2 hover:text-kolping-400'
+                aria-label='YouTube'
+              >
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 24 24'
+                  width='18'
+                  height='18'
+                  fill='currentColor'
+                  aria-hidden='true'
+                >
+                  <path d='M23.498 6.186a3 3 0 0 0-2.118-2.118C19.8 3.5 12 3.5 12 3.5s-7.8 0-9.38.568A3 3 0 0 0 .502 6.186C0 7.766 0 12 0 12s0 4.234.502 5.814a3 3 0 0 0 2.118 2.118C4.2 20.5 12 20.5 12 20.5s7.8 0 9.38-.568a3 3 0 0 0 2.118-2.118C24 16.234 24 12 24 12s0-4.234-.502-5.814zM9.75 15.568V8.432L15.818 12l-6.068 3.568z' />
+                </svg>
+                <span>YouTube</span>
+              </a>
+              <a
+                href='https://www.instagram.com/kolpingjugend_ramsen/'
+                target='_blank'
+                rel='noreferrer'
+                className='inline-flex items-center gap-2 hover:text-kolping-400'
+                aria-label='Instagram'
+              >
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 24 24'
+                  width='18'
+                  height='18'
+                  fill='currentColor'
+                  aria-hidden='true'
+                >
+                  <path d='M7 2h10a5 5 0 0 1 5 5v10a5 5 0 0 1-5 5H7a5 5 0 0 1-5-5V7a5 5 0 0 1 5-5zm0 2a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3H7zm5 3.5a5.5 5.5 0 1 1 0 11 5.5 5.5 0 0 1 0-11zm0 2a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7zM18 6.8a1 1 0 1 1-2 0 1 1 0 0 1 2 0z' />
+                </svg>
+                <span>Instagram</span>
+              </a>
+            </div>
           </div>
         </footer>
       </body>
